c7d448b1a599fff8d0f8ae7fe9006fa630ef18c9
Sam-Si <13261099+Sam-Si@users.noreply.github.com>
2022-09-19 22:30:21 +0530

    Challenge 19 linting_exercise.py

--- contributors/Sam-Si/linting_exercise.py
+++ contributors/Sam-Si/linting_exercise.py
@@ -1,45 +1,6 @@
 """
 Write the most powerful python program
 """
-import sys
-import os
-import re
-import argparse
-import logging
-import logging.config
-import json
-import time
-import datetime
-import random
-import string
-import subprocess
-import multiprocessing
-import threading
-import queue
-import functools
-import itertools
-import collections
-import math
-import statistics
-import decimal
-import fractions
-import urllib.request
-import urllib.parse
-import urllib.error
-import http.client
-import smtplib
-import email.mime.text
-import email.mime.multipart
-import email.mime.application
-import email.mime.image
-import email.mime.audio
-import email.mime.base
-import email.mime.message
-import email.mime.nonmultipart
-import email.mime.text
-import email.mime.multipart
-import email.mime.application
-import email.mime.image
 
 
 def main():
@@ -51,5 +12,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
