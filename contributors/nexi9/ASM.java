06c502ae3a61697d02d1d1038ad0176fe6b149eb
Nayaneeka Mahapatra <mahapatranayaneeka@gmail.com>
2022-09-19 22:41:10 +0530

    Challenge 19:Nayaneeka Mahapatra
    Signed-off-by: Nayaneeka Mahapatra <mahapatranayaneeka@gmail.com>

--- contributors/nexi9/ASM.java
+++ contributors/nexi9/ASM.java
@@ -1,33 +1,8 @@
-public class ASM{
+# Hello world
 
-    public static int asm(String source,String target){
-        int m=source.length();
-        int n=target.length();
-        int[][] mat=new int[m+1][n+1];
-        for(int i=0;i<=m||i<=n;i++){
-            if(i<m){
-                mat[i][0]=i;
-            }
-            if(i<n){
-                mat[0][i]=i;
-            }
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(source.charAt(i-1)==target.charAt(j-1)){
-                    mat[i][j]=mat[i-1][j-1];
-                }else{
-                    mat[i][j]=Math.min(mat[i-1][j-1]+1, Math.min(mat[i][j-1]+1, mat[i-1][j]+1));
-                }
-            }
-        }
-        return mat[m][n];
-    }
-
-    public static void main(String[] args){
-        String s1="aba";
-        String s2="bab";
-        System.out.println(asm(s1, s2));
+class ASM {
+     public static void main(String[] args){
+        System.out.println("Hello World");
 
     }
 }
