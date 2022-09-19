8c436e0090d999600829f7869f59ded6a1b2d02f
Priyanshu Mohanty <priyanshumohanty705@gmail.com>
2022-09-19 22:53:47 +0530

    Challenge 19:Priyanshu Mohanty
    Signed-off-by: Priyanshu Mohanty <priyanshumohanty705@gmail.com>

--- contributors/priyanshu68/index.java
+++ contributors/priyanshu68/index.java
@@ -1,9 +1,11 @@
-class index
-{
-     public static void main(String[] args) {
-        String str ="Hello World";
-        System.out.print(str);
-    }  
-        
-    
+# Hello World
+
+class index {
+
+public static void main(String[] args) {
+    String str = "Hello World";
+    System.out.print(str);
 }
+
+}
+
