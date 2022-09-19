617e1f3b0d296a3945c57a9b947960363adb2417
Rahul Srivastava <srivastavar433@gmail.com>
2022-09-19 23:00:54 +0530

    challenge 19
    Signed-off-by: Rahul Srivastava <srivastavar433@gmail.com>

---
+++ contributors/rahulsrivastava1/Hello_world.cpp
@@ -0,0 +1,14 @@
+// C++ program to display "Hello World"
+  
+// Header file for input output functions
+#include <iostream>
+using namespace std;
+  
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World";
+  
+    return 0;
+}
