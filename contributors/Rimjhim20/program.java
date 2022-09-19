b3647db052d4f0620cb31a3f7897ad5c4736325c
Shivay Lamba <shivaylamba@gmail.com>
2022-09-19 15:05:02 +0530

    javaprogram
    Squashed all commits
    
    Signed-off-by: Rimjhim <kumaririmjhim720@gmail.com>

---
+++ contributors/Rimjhim20/program.java
@@ -0,0 +1,12 @@
+package contributors.Rimjhim20;
+
+public class program {
+    public static void main(String[] args) {
+        int x = 20;
+
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+    }
+}
