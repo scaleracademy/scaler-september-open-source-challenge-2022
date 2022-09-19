6703e17da05ec04cc14ce446674004ae946a2b72
Akshatpattiwar512 <akshatpattiwar512@gmail.com>
2022-09-19 21:04:21 +0530

    Sample Python script
    Signed-off-by: Akshatpattiwar512 <akshatpattiwar512@gmail.com>

--- contributors/Akshatpattiwar512/code.py
+++ contributors/Akshatpattiwar512/code.py
@@ -1,2 +1,20 @@
-a=5
-print(a)
+"""
+Sample python script
+"""
+
+print("before import")
+import math
+
+print("before function_a")
+def function_a():
+    print("Function A")
+
+print("before function_b")
+def function_b():
+    print("Function B {}".format(math.sqrt(100)))
+
+print("before __name__ guard")
+if __name__ == '__main__':
+    function_a()
+    function_b()
+print("after __name__ guard")
