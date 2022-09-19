2c7d1d4cd435988e5e8c7d6cdd69d264f884310d
Rimjhim <kumaririmjhim720@gmail.com>
2022-09-19 19:00:07 +0530

    javaprogram
    Signed-off-by: Rimjhim <kumaririmjhim720@gmail.com>

--- contributors/Rimjhim20/program.java
+++ contributors/Rimjhim20/program.java
@@ -3,7 +3,6 @@ package contributors.Rimjhim20;
 public class program {
     public static void main(String[] args) {
         int x = 20;
-
         int y = 18;
         if (x > y) {
             System.out.println("x is greater than y");
