0372d2a35068fff64c67dc76647089d3dc501613
imhardikdesai <hp676913@gmail.com>
2022-09-19 12:51:53 +0530

    added gists file
    Signed-off-by: imhardikdesai <hp676913@gmail.com>

---
+++ contributors/imhardikdesai/gist-solutions.md
@@ -0,0 +1,2 @@
+SDLC : https://gist.github.com/imhardikdesai/d7ffb00f8dda5b029237c8ac8079500a
+Greatest Number : https://gist.github.com/imhardikdesai/3176c7589f3047df69038f5f5ae3956f
---
+++ contributors/imhardikdesai/largest.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 14
+num3 = 12
+
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    largest = num2
+else:
+    largest = num3
+
+print("The largest number is", largest)
