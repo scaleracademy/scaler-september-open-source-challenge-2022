51bd4b57c7634c5b7294c2721c03afeccdc74c2e
krati yadav <kratiy.5@gmail.com>
2022-09-19 21:46:19 +0530

    Fixed lint errors
    Signed-off-by: krati yadav <kratiy.5@gmail.com>

--- contributors/krati5/gist-solutions.md
+++ contributors/krati5/gist-solutions.md
@@ -1,6 +1,6 @@
 
-## Software Development Topic gist
+# Software Development Topic gist
 https://gist.github.com/krati5/d69e323a7b2e72ed9bc9505f15b0f524
 
-## Code snippet gist
+# Code snippet gist
 https://gist.github.com/krati5/c671282194c9602ed20be3b5876cf3a9
---
+++ contributors/krati5/knapsack_problem.py
@@ -0,0 +1,38 @@
+"""main function"""
+def knapsack_problem(weight_arr, value_arr, w_cap, n_items):
+    """
+    Returns maximum output
+    """
+
+    dp_dict = [[-1 for j in range(w_cap + 1)] for i in range(n_items + 1)]
+
+    for j in range(w_cap + 1):
+        dp_dict[0][j] = 0
+
+    for i in range(n_items + 1):
+        dp_dict[i][0] = 0
+
+    for i in range(1, n_items + 1):
+        for j in range(1, w_cap + 1):
+
+            if weight_arr[i - 1] <= j:
+                dp_dict[i][j] = max(
+                    value_arr[i - 1] + dp_dict[i - 1][j - weight_arr[i - 1]], dp_dict[i - 1][j]
+                )
+
+            else:
+                dp_dict[i][j] = dp_dict[i - 1][j]
+
+    return dp_dict[n_items][w_cap]
+
+
+if __name__ == "__main__":
+    value = [60, 100, 120]
+    weight = [10, 20, 30]
+    N = len(value)
+
+    W = 50  # Capacity
+    print(
+        "Dynamic Programming Top-Down Approach: ",
+        knapsack_problem(weight, value, W, N),
+    )
