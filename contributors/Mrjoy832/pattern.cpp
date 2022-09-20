03e3d88a2d3caa3bd9a5dddc85c00cdaf81d696c
TRIDIB BAG <tridibbag56@gmail.com>
2022-09-20 19:42:44 +0530

    challenge-19 file
    Signed-off-by: TRIDIB BAG <tridibbag56@gmail.com>

--- contributors/Mrjoy832/pattern.cpp
+++ contributors/Mrjoy832/pattern.cpp
@@ -3,18 +3,21 @@
  */
 #include <iostream>
 
-int main() {
+int main()
+{
   int rows;
 
   std::cout << "Enter number of rows: ";
   std::cin >> rows;
 
-   for (int i = rows; i >= 1; --i) {
-     for (int j = 1; j <= i; ++j) {
-         std::cout << j << " ";
+  for (int i = rows; i >= 1; --i)
+  {
+    for (int j = 1; j <= i; ++j)
+    {
+      std::cout << j << " ";
     }
     std::cout << std::endl;
   }
-  
+
   return 0;
 }
