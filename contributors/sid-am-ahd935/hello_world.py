f9811a4272eae8d506135603f6c2ae4374365352
Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>
2022-09-20 15:01:57 +0530

    Linted hello_world.py
    Signed-off-by: Aman Ahmed Siddiqui <aasiddiqui40@gmail.com>

--- contributors/sid-am-ahd935/hello_world.py
+++ contributors/sid-am-ahd935/hello_world.py
@@ -1,12 +1,37 @@
+"""
+This is just a demo program for linting
+"""
+
 class World:
-    def __init__(self, text):
+    """
+    And this is a demo class for printing stuff
+    """
+    def __init__(self, text= "World"):
+        """
+        Constructor Doc String
+        """
         self.text = text
 
     def __str__(self):
+        """
+        Class Built-in Method Docstring
+        """
         return "Hello " + self.text + "!!!"
+
+    def print(self, text):
+        """
+        Global Method for calling.
+        """
+        print("This is the text:", text)
+
+
+def main():
+    """
+    Main function to run script
+    """
+    print(World('Aman'))
+    World().print('Peanuts in Pumpkins')
 
 
 if __name__ == "__main__":
-    print(World('Aman'))
-
-
+    main()
