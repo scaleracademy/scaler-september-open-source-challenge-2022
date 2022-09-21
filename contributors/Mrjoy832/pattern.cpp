4dd0c395c7854d0ab35f05b66893f6cc6a27be61
TRIDIB BAG <tridibbag56@gmail.com>
2022-09-21 14:19:01 +0530

    linter error fix
    Signed-off-by: TRIDIB BAG <tridibbag56@gmail.com>

--- contributors/Mrjoy832/pattern.cpp
+++ contributors/Mrjoy832/pattern.cpp
@@ -3,17 +3,14 @@
  */
 #include <iostream>
 
-int main()
-{
+int main() {
   int rows;
 
   std::cout << "Enter number of rows: ";
   std::cin >> rows;
 
-  for (int i = rows; i >= 1; --i)
-  {
-    for (int j = 1; j <= i; ++j)
-    {
+  for (int i = rows; i >= 1; --i) {
+    for (int j = 1; j <= i; ++j) {
       std::cout << j << " ";
     }
     std::cout << std::endl;
