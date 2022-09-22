ea2eef8fe4be6f741a6baf650699bd37fc9333a1
pratham <prathambagadiya6606@gmail.com>
2022-09-22 22:26:13 +0530

    qwery
    Signed-off-by: pratham <prathambagadiya6606@gmail.com>

--- contributors/prathambagadiya/armstrongnum.py
+++ contributors/prathambagadiya/armstrongnum.py
@@ -1,17 +1,9 @@
-lower = 100
-upper = 2000
+x = 5
+y = 10
 
-for num in range(lower, upper + 1):
+temp = x
+x = y
+y = temp
 
-    order = len(str(num))
-
-    sum = 0
-
-    temp = num
-    while temp > 0:
-        digit = temp % 10
-        sum += digit ** order
-        temp //= 10
-
-    if num == sum:
-        print(num)
+print("The value of x after swapping: {}".format(x))
+print("The value of y after swapping: {}".format(y))
--- contributors/prathambagadiya/gist-solution.md
+++ contributors/prathambagadiya/gist-solution.md
@@ -1,2 +1,3 @@
+# Gist Solutions
 [Threading](https://gist.github.com/prathambagadiya/0fd09dd1c4362c0d78dc535677bc136b#file-threading-md)
 [Counter javascript](https://gist.github.com/prathambagadiya/0fd09dd1c4362c0d78dc535677bc136b#file-conter-java)
