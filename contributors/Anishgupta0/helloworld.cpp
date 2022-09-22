03cc14a41c6ac267954abc8c25316ede451c3d77
Anish kumar gupta <86661305+Anishgupta0@users.noreply.github.com>
2022-09-22 23:14:36 +0530

    lint error fix
    Signed-off-by: Anish kumar gupta <86661305+Anishgupta0@users.noreply.github.com>

--- contributors/Anishgupta0/helloworld.cpp
+++ contributors/Anishgupta0/helloworld.cpp
@@ -1,6 +1,9 @@
+/*    Copyright (c) 2022, <Anish gupta>
+ *    All rights reserved
+ */
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+  std::cout << "Hello World!";
+  return 0;
 }
